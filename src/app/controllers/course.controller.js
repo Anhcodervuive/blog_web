0401f61a225fa66c49be6bd393e4f359c1f86165
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2024-12-29 12:17:06 +0700

    test husky p5

--- src/app/controllers/course.controller.js
+++ src/app/controllers/course.controller.js
@@ -26,12 +26,12 @@ class CourseController {
 
     async store(req, res, next) {
         try {
-            const formData = req.body
+            const formData = req.body;
             const course = await Course.create(formData);
-            res.redirect(301, "/")
+            res.redirect(301, '/');
         } catch (error) {
             console.error(error)
-            res.status(500).send("Error occurred while creating course");
+            res.status(500).send('Error occurred while creating course')
         }
     }
 }
