e38798fcd6e59bc10968b783603408bc7cf043c1
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-01-13 16:05:35 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
+const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
@@ -9,7 +10,7 @@ const route = require('./routes')
 const db = require('./config/db')
 
 // Connect to DB
-db.connect();
+db.connect()
 
 // HTTP Loggers
 app.use(morgan('combined'))
@@ -20,8 +21,9 @@ app.engine('hbs', exphbs.engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '/resource/views'))
 
-// Middleware handler data with post method from form 
+// Middleware handler data with post method from form
 app.use(express.urlencoded())
+app.use(methodOverride('_method'))
 
 route(app)
 
